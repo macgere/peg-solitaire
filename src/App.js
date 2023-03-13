7a90a3284de7129bd45754a114ea9cab0b17e5e4
Marc G <mac_gere@yahoo.com>
2023-03-13 15:24:57 -0500

    begginings of peg solitaire game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [position, setPosition] = useState([][0])
+
+  const handleClick = (event) => {
+      setPosition(event.currentTarget.index)
+  };
+
+
+  const boardRow1 = [1,1,1,1,1]
+  const boardRow2 = [1,1,0,1,1]
+  const boardRow3 = [1,1,1,1,1]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row'>
+        {boardRow1.map((x) => {
+          if (x == 1)
+            return <button key={x.id} className='peg' onClick={handleClick(index)}>x</button>
+          if (x == 0)
+            return <div className='space'>O</div>
+        })}
+        </div>
+        <div className='row'>
+          {boardRow2.map((x) => {
+            if (x == 1)
+              return <button key={x.id} className='peg' onClick={handleClick(index)}>x</button>
+            if (x == 0)
+              return <div className='space'>O</div>
+          })}
+        </div>
+        <div className='row'>
+        {boardRow3.map((x) => {
+          if (x == 1) 
+            return <button key={x.id} className='peg' onClick={handleClick(index)}>x</button>
+          if (x == 0) 
+            return <div className='space'>O</div>
+        })}
+      </div>
     </div>
   );
 }
