a7013d4a74f077128d7403c38811b8b29fefdae3
Marc G <mac_gere@yahoo.com>
2023-03-14 14:11:47 -0500

    added indexing to the map

--- src/App.js
+++ src/App.js
@@ -4,41 +4,62 @@ import { useState } from 'react';
 
 function App() {
 
-  const handleClick = () => {
-
+  const handleClick = (index) => {
+    if (index == 3) {
+      console.log('success!')
+    }
   };
 
 
-  const boardRow1 = [1,1,1,1,1]
-  const boardRow2 = [1,1,0,1,1]
-  const boardRow3 = [1,1,1,1,1]
+  const boardRow0 = [1,1,1]
+  const boardRow1 = [1,1,1,1,1,1,1]
+  const boardRow2 = [1,1,1,0,1,1,1]
+  const boardRow3 = [1,1,1,1,1,1,1]
+  const boardRow4 = [1,1,1]
+
 
   return (
     <div className="App">
-      <div className='row'>
-        {boardRow1.map((x) => {
+            <div className='row'>
+        {boardRow0.map((x, index) => {
           if (x == 1)
-            return <button key={x.id} className='peg' onClick={handleClick()}>x</button>
+            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+          if (x == 0)
+            return <div className='space'>O</div>
+        })}
+        </div>
+      <div className='row'>
+        {boardRow1.map((x, index) => {
+          if (x == 1)
+            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
           if (x == 0)
             return <div className='space'>O</div>
         })}
         </div>
         <div className='row'>
-          {boardRow2.map((x) => {
+          {boardRow2.map((x, index) => {
             if (x == 1)
-              return <button key={x.id} className='peg' onClick={handleClick()}>x</button>
+              return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
             if (x == 0)
               return <div className='space'>O</div>
           })}
         </div>
         <div className='row'>
-        {boardRow3.map((x) => {
+        {boardRow3.map((x, index) => {
           if (x == 1) 
-            return <button key={x.id} className='peg' onClick={handleClick()}>x</button>
+            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
           if (x == 0) 
             return <div className='space'>O</div>
         })}
       </div>
+      <div className='row'>
+        {boardRow4.map((x, index) => {
+          if (x == 1)
+            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+          if (x == 0)
+            return <div className='space'>O</div>
+        })}
+        </div>
     </div>
   );
 }
