8534f7dd826ca4f4afd677bfad3fdba7d09d7ccc
Marc G <mac_gere@yahoo.com>
2023-03-15 13:46:00 -0500

    reduced redundancy, mapped through a map instead

--- src/App.js
+++ src/App.js
@@ -1,63 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const handleClick = (index) => {
-    if (index == 3) {
-      console.log('success!')
-    }
+  const [target, setTarget] = useState(0)
+
+  const handleClick = (number) => {
+    boardRow0.filter((number) => number = 0)
+    setTarget(number)
   };
 
+  useEffect(()=>{
+    console.log(target)
+  }, [target])
 
-  const boardRow0 = [1,1,1]
-  const boardRow1 = [1,1,1,1,1,1,1]
-  const boardRow2 = [1,1,1,0,1,1,1]
-  const boardRow3 = [1,1,1,1,1,1,1]
-  const boardRow4 = [1,1,1]
+
+  const boardRow0 = [1,2,3]
+  const boardRow1 = [1,2,3,4,5,6,7]
+  const boardRow2 = [1,2,3,0,5,6,7]
+  const boardRow3 = [1,2,3,4,5,6,7]
+  const boardRow4 = [1,2,3]
+  const board = [boardRow0, boardRow1, boardRow2, boardRow3, boardRow4]
 
 
   return (
     <div className="App">
-            <div className='row'>
-        {boardRow0.map((x, index) => {
-          if (x == 1)
-            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+            {/* <div className='row'>
+        {boardRow0.map((x) => {
+          if (x !== 0)
+            return <button key={x} className='peg' onClick={() => handleClick(x)}>x</button>
           if (x == 0)
-            return <div className='space'>O</div>
+            return <div key={x} className='space'>O</div>
         })}
         </div>
       <div className='row'>
-        {boardRow1.map((x, index) => {
-          if (x == 1)
-            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+        {boardRow1.map((x) => {
+          if (x !== 0)
+            return <button key={x} className='peg' onClick={() => handleClick(x)}>x</button>
           if (x == 0)
             return <div className='space'>O</div>
         })}
         </div>
         <div className='row'>
-          {boardRow2.map((x, index) => {
-            if (x == 1)
-              return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+          {boardRow2.map((x) => {
+            if (x !== 0)
+              return <button key={x} className='peg' onClick={() => handleClick(x)}>x</button>
             if (x == 0)
               return <div className='space'>O</div>
           })}
         </div>
         <div className='row'>
-        {boardRow3.map((x, index) => {
-          if (x == 1) 
-            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+        {boardRow3.map((x) => {
+          if (x !== 0) 
+            return <button key={x} className='peg' onClick={() => handleClick(x)}>x</button>
           if (x == 0) 
             return <div className='space'>O</div>
         })}
       </div>
       <div className='row'>
-        {boardRow4.map((x, index) => {
-          if (x == 1)
-            return <button key={index} className='peg' onClick={() => handleClick(index)}>x</button>
+        {boardRow4.map((x) => {
+          if (x !== 0)
+            return <button key={x} className='peg' onClick={() => handleClick(x)}>x</button>
           if (x == 0)
             return <div className='space'>O</div>
+        })}
+        </div> */}
+        <div>
+        {board.map((x) => {
+          return x.map((y) => {
+            if (y !== 0)
+              return <button key={y} className='peg' onClick={() => handleClick(y)}>x</button>
+            if (y == 0)
+              return <div className='space'>O</div>
+          })
         })}
         </div>
     </div>
