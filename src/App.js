7915baed593fd7517a4c2959ab00bb9796482902
Marc G <mac_gere@yahoo.com>
2023-03-15 15:19:59 -0500

    added keys to spaces

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [target, setTarget] = useState(0)
+  const [previousTarget, setPreviousTarget] = useState(0)
 
   const handleClick = (number) => {
     boardRow0.filter((number) => number = 0)
@@ -12,28 +13,32 @@ function App() {
   };
 
   useEffect(()=>{
+    setPreviousTarget(target)
     console.log(target)
+    console.log(previousTarget)
   }, [target])
 
 
-  const boardRow0 = [0,0,1,1,1,0,0]
-  const boardRow1 = [1,1,1,1,1,1,1]
-  const boardRow2 = [1,1,1,0,1,1,1]
-  const boardRow3 = [1,1,1,1,1,1,1]
-  const boardRow4 = [0,0,1,1,1,0,0]
+  const boardRow0 = [0,0,1,1,1,0,0,-1]
+  const boardRow1 = [1,1,1,1,1,1,1,-1]
+  const boardRow2 = [1,1,1,0,1,1,1,-1]
+  const boardRow3 = [1,1,1,1,1,1,1,-1]
+  const boardRow4 = [0,0,1,1,1,0,0,-1]
   const board = [boardRow0, boardRow1, boardRow2, boardRow3, boardRow4]
 
 
   return (
     <div className="App">
-        <div>
+        <div className='row'>
         {board.map((x, rowX) => {
           let row = rowX.toString()
           return x.map((y, position) => {
             let column = position.toString()
-            if (y !== 0)
-              return <button key={row + column} className='peg' onClick={() => handleClick((row + column).toString())}>x</button>
-            else return <div className='space'>'  '</div>
+            if (y > 0)
+              return <button key={row + column} className='peg' onClick={() => handleClick((row + column))}>x</button>
+            if (y === -1)
+              return <><br></br></>
+            else return <button key={row + column} className='space' onClick={() => handleClick((row + column))}>x</button>
           })
         })}
         </div>
